dd38d464121180e9a53ec634d4794fc140c0eb67
dmytro-b-fs <dmytro.burakov@gmail.com>
2026-07-24 21:47:59 +0300

    hwts1-1

--- src/task-8.ts
+++ src/task-8.ts
@@ -6,11 +6,11 @@ interface Post{
     body: string
 }
 
-async function fetchPosts() {
-  const response = await axios.get(
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
-  return response.data as Post[];
+  return response.data;
 }
 
 fetchPosts().then((posts) => {
