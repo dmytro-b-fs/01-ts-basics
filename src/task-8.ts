6e957ded44c7817cc4dd6f1ee03528a3b0a9e877
dmytro-b-fs <dmytro.burakov@gmail.com>
2026-07-24 21:41:23 +0300

    hwts1

---
+++ src/task-8.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+interface Post{
+    id: number
+    title: string
+    body: string
+}
+
+async function fetchPosts() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  return response.data as Post[];
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
